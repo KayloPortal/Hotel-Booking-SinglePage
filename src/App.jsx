a74e24d1f5131a409860c245e7dbaea435e1c3b2
kayloportal <pinobaxp@gmail.com>
2024-03-21 12:55:36 +0330

    added/ container style

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import './App.css'
-import Header from './components/Sections/Header/Header'
-import Hotels from './components/Sections/Hotels/Hotels'
-import Map from './components/Sections/Map/Map'
+import "./App.css";
+import Header from "./components/Sections/Header/Header";
+import Hotels from "./components/Sections/Hotels/Hotels";
+import Map from "./components/Sections/Map/Map";
 
 function App() {
-
   return (
-   <>
-    <Header />
-    <main>
-      <Hotels />
-      <Map />
-    </main>
-   </>
-  )
+    <>
+      <Header />
+      <main className="container main">
+        <Hotels />
+        <Map />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
