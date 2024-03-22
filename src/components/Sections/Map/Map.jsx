5e890933ee9ee5f85c862c12f7193af70c285a74
kayloportal <pinobaxp@gmail.com>
2024-03-22 15:46:36 +0330

    added/ Map JSX & CSS

--- src/components/Sections/Map/Map.jsx
+++ src/components/Sections/Map/Map.jsx
@@ -1,9 +1,41 @@
-
+import { useState } from "react";
+import { Marker, Popup } from "react-leaflet";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { useMap } from "react-leaflet/hooks";
+import L from "leaflet"
 
 function Map() {
+  // const [mapCenter, setMapCenter] = useState([51.505, -0.09])
   return (
-    <div>Map</div>
-  )
+    <div className="map-container">
+      <MapContainer
+        style={{ height: "100%" }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker icon={new L.Icon({
+          iconUrl: "leaf-green.png",
+          shadowUrl: "leaf-shadow.png",
+          iconSize: [38, 95],
+          shadowSize: [50, 64],
+          iconAnchor: [22, 94],
+          shadowAnchor: [4, 62],
+          popupAnchor: [-3, -76],
+          className: "marker"
+        })} position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
